0e338a3e890391dd0c9a46ebdcb399e049843fbc
vcoppe <vianney.coppe@gmail.com>
2024-06-19 20:13:13 +0200

    deploy to pages

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -14,6 +14,10 @@ const config = {
         }),
         paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        },
+        prerender: {
+            entries: ['/'],
+            crawl: true,
         }
     }
 };
