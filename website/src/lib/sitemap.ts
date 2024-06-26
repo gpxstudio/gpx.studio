8b6057ce323d84e4864f04be64d141eda2f83a5c
vcoppe <vianney.coppe@gmail.com>
2024-06-26 22:52:38 +0200

    add namespace

--- website/src/lib/sitemap.ts
+++ website/src/lib/sitemap.ts
@@ -7,7 +7,7 @@ function getURL(lang: string, path: string = '/') {
 
 function generateSitemap() {
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">` +
 
         Object.keys(languages).map((lang) => `
     <url>
