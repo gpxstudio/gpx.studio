278be5c794e31aa1badce4823bc9d407ef02311c
vcoppe <vianney.coppe@gmail.com>
2024-06-27 23:45:33 +0200

    fix meta links

--- website/src/routes/[...language]/+page.svelte
+++ website/src/routes/[...language]/+page.svelte
@@ -10,17 +10,8 @@
 	<meta name="description" content={$_('metadata.description')} />
 	<meta property="og:title" content="gpx.studio — {$_('metadata.app_title')}" />
 	<meta property="og:description" content={$_('metadata.description')} />
-	<meta property="og:image" content="{base}/og_logo.png" />
-	<meta property="og:url" content="{base}/" />
-	<meta property="og:type" content="website" />
-	<meta property="og:site_name" content="gpx.studio" />
-	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="gpx.studio — {$_('metadata.app_title')}" />
 	<meta name="twitter:description" content={$_('metadata.description')} />
-	<meta name="twitter:image" content="{base}/og_logo.png" />
-	<meta name="twitter:url" content="{base}/" />
-	<meta name="twitter:site" content="@gpxstudio" />
-	<meta name="twitter:creator" content="@gpxstudio" />
 
 	<link rel="alternate" hreflang="x-default" href="{base}/" />
 	{#each Object.keys(languages) as lang}
