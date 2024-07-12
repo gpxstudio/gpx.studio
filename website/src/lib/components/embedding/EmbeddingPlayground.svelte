959aa9c8ed8d87fafadfc8be0b8340989d2271f7
vcoppe <vianney.coppe@gmail.com>
2024-07-12 15:56:35 +0200

    fix embedding URL

--- website/src/lib/components/embedding/EmbeddingPlayground.svelte
+++ website/src/lib/components/embedding/EmbeddingPlayground.svelte
@@ -216,7 +216,7 @@
 			</Label>
 			<pre class="bg-primary text-primary-foreground p-3 rounded-md whitespace-normal break-all">
                 <code class="language-html">
-                    {`<iframe src="https://gpx.studio${base}/embed?${JSON.stringify(getCleanedEmbeddingOptions(options))}${hash}" width="100%" height="600px" frameborder="0" />`}
+                    {`<iframe src="https://gpx.studio${base}/embed?options=${encodeURIComponent(JSON.stringify(getCleanedEmbeddingOptions(options)))}${hash}" width="100%" height="600px" frameborder="0" />`}
                 </code>
             </pre>
 		</fieldset>
