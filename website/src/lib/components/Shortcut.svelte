cfc537a59c33dc33c3fe5d610c7f6c33ebbd4e87
vcoppe <vianney.coppe@gmail.com>
2024-07-26 11:40:18 +0200

    show ctrl shortcuts for safari, closes #31

--- website/src/lib/components/Shortcut.svelte
+++ website/src/lib/components/Shortcut.svelte
@@ -8,14 +8,16 @@
 	export let click: boolean = false;
 
 	let isMac = false;
+	let isSafari = false;
 
 	onMount(() => {
 		isMac = navigator.userAgent.toUpperCase().indexOf('MAC') >= 0;
+		isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 	});
 </script>
 
 <span class="ml-auto pl-2 text-xs tracking-widest text-muted-foreground"
-	>{shift ? '⇧' : ''}{ctrl ? (isMac ? '⌘' : $_('menu.ctrl') + '+') : ''}{key}{click
+	>{shift ? '⇧' : ''}{ctrl ? (isMac && !isSafari ? '⌘' : $_('menu.ctrl') + '+') : ''}{key}{click
 		? $_('menu.click')
 		: ''}</span
 >
