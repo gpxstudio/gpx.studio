1f92e377adce5a960acf76a5f474c92a982f527e
vcoppe <vianney.coppe@gmail.com>
2024-08-05 14:44:40 +0200

    add languages

--- website/src/lib/languages.ts
+++ website/src/lib/languages.ts
@@ -1,3 +1,7 @@
 export const languages: Record<string, string> = {
     'en': 'English',
+    'es': 'Español',
+    'fr': 'Français',
+    'nl': 'Nederlands',
+    'pt-BR': 'Português (Brasil)',
 };
--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,28 +1,11 @@
 <script lang="ts">
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
-	import { isLoading, locale, _ } from 'svelte-i18n';
+	import { isLoading, _ } from 'svelte-i18n';
 	import { page } from '$app/stores';
 	import Head from '$lib/components/Head.svelte';
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import { languages } from '$lib/languages';
-	import { browser } from '$app/environment';
-	import { goto } from '$app/navigation';
-	import { base } from '$app/paths';
-
-	$: if ($page.params.language === '' && $locale !== 'en') {
-		locale.set('en');
-	} else if ($page.params.language) {
-		let lang = $page.params.language.replace('/', '');
-		if ($locale !== lang) {
-			if (languages.hasOwnProperty(lang)) {
-				locale.set(lang);
-			} else if (browser) {
-				goto(`${base}/404`);
-			}
-		}
-	}
 
 	const appRoutes = ['/[...language]/app', '/[...language]/embed'];
 
