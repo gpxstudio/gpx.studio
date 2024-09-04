2612eb2e91dbcbde1e7286742aee7b3aaf902b16
vcoppe <vianney.coppe@gmail.com>
2024-09-04 13:36:19 +0200

    update selected routing profile label when locale changes

--- website/src/lib/components/toolbar/tools/routing/Routing.ts
+++ website/src/lib/components/toolbar/tools/routing/Routing.ts
@@ -24,7 +24,7 @@ export const routingProfileSelectItem = writable({
 });
 
 derived([routingProfile, locale, isLoading], ([profile, l, i]) => [profile, l, i]).subscribe(([profile, l, i]) => {
-    if (!i && profile !== '' && profile !== get(routingProfileSelectItem).value && l !== null) {
+    if (!i && profile !== '' && (profile !== get(routingProfileSelectItem).value || get(_)(`toolbar.routing.activities.${profile}`) !== get(routingProfileSelectItem).label) && l !== null) {
         routingProfileSelectItem.update((item) => {
             item.value = profile;
             item.label = get(_)(`toolbar.routing.activities.${profile}`);
