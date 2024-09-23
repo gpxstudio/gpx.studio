c02d96e90f0e1397d516f297d02d5ab35efc0af7
vcoppe <vianney.coppe@gmail.com>
2024-09-23 14:55:38 +0200

    fix rounded docs img on safari

--- website/src/lib/components/docs/DocsImage.svelte
+++ website/src/lib/components/docs/DocsImage.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="flex flex-col items-center py-6 w-full">
-	<div class="rounded-md overflow-clip shadow-xl mx-auto">
+	<div class="rounded-md overflow-hidden overflow-clip shadow-xl mx-auto">
 		<enhanced:img {src} {alt} class="w-full max-w-3xl" />
 	</div>
 	<p class="text-center text-sm text-muted-foreground mt-2">{alt}</p>
