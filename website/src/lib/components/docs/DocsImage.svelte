9eb716e36c02a85623aceb036f71b5fa73cf835d
vcoppe <vianney.coppe@gmail.com>
2024-09-24 13:50:44 +0200

    change name of img parameter

--- website/src/lib/components/docs/DocsImage.svelte
+++ website/src/lib/components/docs/DocsImage.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	export let img: 'getting-started/interface' | 'tools/routing' | 'tools/split';
+	export let src: 'getting-started/interface' | 'tools/routing' | 'tools/split';
 	export let alt: string;
 </script>
 
 <div class="flex flex-col items-center py-6 w-full">
 	<div class="rounded-md overflow-hidden overflow-clip shadow-xl mx-auto">
-		{#if img === 'getting-started/interface'}
+		{#if src === 'getting-started/interface'}
 			<enhanced:img
 				src="/src/lib/assets/img/docs/getting-started/interface.png"
 				{alt}
 				class="w-full max-w-3xl"
 			/>
-		{:else if img === 'tools/routing'}
+		{:else if src === 'tools/routing'}
 			<enhanced:img
 				src="/src/lib/assets/img/docs/tools/routing.png"
 				{alt}
 				class="w-full max-w-3xl"
 			/>
-		{:else if img === 'tools/split'}
+		{:else if src === 'tools/split'}
 			<enhanced:img src="/src/lib/assets/img/docs/tools/split.png" {alt} class="w-full max-w-3xl" />
 		{/if}
 	</div>
