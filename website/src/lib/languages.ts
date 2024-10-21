798d8e7a14b53ba6a285fce788a74886891789c9
vcoppe <vianney.coppe@gmail.com>
2024-10-21 23:27:19 +0200

    add german

--- website/src/lib/languages.ts
+++ website/src/lib/languages.ts
@@ -1,6 +1,7 @@
 export const languages: Record<string, string> = {
     'en': 'English',
     'es': 'Español',
+    'de': 'Deutsch',
     'fr': 'Français',
     'nl': 'Nederlands',
     'pt-BR': 'Português (Brasil)',
