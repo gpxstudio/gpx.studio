b5bf06b37a1e958d2b2e895a9a359fe441c8d503
vcoppe <vianney.coppe@gmail.com>
2025-02-15 13:09:41 +0100

    format file

--- website/src/routes/+layout.server.ts
+++ website/src/routes/+layout.server.ts
@@ -1,30 +1,30 @@
 import { guides } from '$lib/components/docs/docs.js';
 
 function getModule(language: string | undefined, guide: string) {
-	language = language ?? 'en';
-	let subguide = undefined;
-	if (guide.includes('/')) {
-		[guide, subguide] = guide.split('/');
-	}
-	return subguide
-		? import(`./../lib/docs/${language}/${guide}/${subguide}.mdx`)
-		: import(`./../lib/docs/${language}/${guide}.mdx`);
+    language = language ?? 'en';
+    let subguide = undefined;
+    if (guide.includes('/')) {
+        [guide, subguide] = guide.split('/');
+    }
+    return subguide
+        ? import(`./../lib/docs/${language}/${guide}/${subguide}.mdx`)
+        : import(`./../lib/docs/${language}/${guide}.mdx`);
 }
 
 export async function load({ params }) {
-	const { language } = params;
+    const { language } = params;
 
-	const guideTitles: Record<string, string> = {};
-	for (let guide of Object.keys(guides)) {
-		guideTitles[guide] = (await getModule(language, guide)).metadata.title;
-		for (let subguide of guides[guide]) {
-			guideTitles[`${guide}/${subguide}`] = (
-				await getModule(language, `${guide}/${subguide}`)
-			).metadata.title;
-		}
-	}
+    const guideTitles: Record<string, string> = {};
+    for (let guide of Object.keys(guides)) {
+        guideTitles[guide] = (await getModule(language, guide)).metadata.title;
+        for (let subguide of guides[guide]) {
+            guideTitles[`${guide}/${subguide}`] = (
+                await getModule(language, `${guide}/${subguide}`)
+            ).metadata.title;
+        }
+    }
 
-	return {
-		guideTitles
-	};
+    return {
+        guideTitles,
+    };
 }
