24ffdb5d9cf1512cba91607f40d15b04ecfe16e4
Anthony DePasquale <anthony@depasquale.org>
2025-02-25 11:51:15 +0100

    Fix eslint and VS Code config

---
+++ gpx/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
+    },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true,
+    },
+};
