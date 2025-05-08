7e8f1acf67fec1a01042f96bf5cd2d7f695aab5d
vcoppe <vianney.coppe@gmail.com>
2025-05-08 18:54:46 +0200

    remove apple touch icon

--- website/src/hooks.server.js
+++ website/src/hooks.server.js
@@ -39,7 +39,6 @@ export async function handle({ event, resolve }) {
     <meta name="twitter:site" content="@gpxstudio" />
     <meta name="twitter:creator" content="@gpxstudio" />
     <link rel="alternate" hreflang="x-default" href="https://gpx.studio${getURLForLanguage('en', path)}" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
     <link rel="manifest" href="/${language}.manifest.webmanifest" />`;
 
     for (let lang of Object.keys(languages)) {
