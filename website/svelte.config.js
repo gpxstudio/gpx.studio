88471ee698639ddcb9e7b346b5b7ec7f9b98197d
Hugo Heneault <hugo@ownweb.fr>
2025-12-30 15:43:57 +0100

    feat: allow passing a custom basePath

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
